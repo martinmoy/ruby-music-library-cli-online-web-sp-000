6c39d4db653cd27933367a3944d25bbf68d0ef72
Martin Moy <martinmoy1991@gmail.com>
2020-07-28 17:41:53 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,67 @@
+
+class Song
+  attr_accessor :name, :artist, :genre
+  @@all =[]
+
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist=artist if artist
+    self.genre=genre if genre
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(song)
+    song = Song.new(song)
+    song.save
+    song
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    if !(genre.songs.include?(self))
+    genre.songs << self
+    end
+  end
+
+  def self.find_by_name(name)
+    @@all.find {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create(name)
+  end
+
+  def self.new_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name, song_name, genre_name = parts.first, parts[1], parts[2].gsub(".mp3", "")
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+    self.new(song_name, artist, genre)
+  end
+
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    song.save
+  end
+
+
+
+
+
+end
